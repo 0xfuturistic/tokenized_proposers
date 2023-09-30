4bb21eb96a2361a9e954d3831099120963a57780
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-30 14:28:27 +0900

    update example VRGDA

--- src/examples/VRGDA.sol
+++ src/examples/VRGDA.sol
@@ -11,8 +11,8 @@ contract VRGDA is ProposerManager, LinearVRGDA {
 
     uint256 public immutable startTime = block.timestamp; // When VRGDA sales begun.
 
-    constructor(address l2OutputOracleAddr)
-        ProposerManager(l2OutputOracleAddr)
+    constructor(address l2OutputOracle, address erc6551Registry, address proposerAccountImpl)
+        ProposerManager(l2OutputOracle, erc6551Registry, proposerAccountImpl)
         LinearVRGDA(
             69.42e18, // Target price.
             0.31e18, // Price decay percent.
@@ -20,7 +20,18 @@ contract VRGDA is ProposerManager, LinearVRGDA {
         )
     {}
 
-    function acquireProposer() external override returns (uint256 id) {
-        _safeMint(msg.sender, id);
+    function acquireProposer() public payable override returns (uint256 tokenId) {
+        unchecked {
+            // Note: By using toDaysWadUnsafe(block.timestamp - startTime) we are establishing that 1 "unit of time" is 1 day.
+            uint256 price = getVRGDAPrice(toDaysWadUnsafe(block.timestamp - startTime), tokenId = totalSold++);
+
+            require(msg.value >= price, "UNDERPAID"); // Don't allow underpaying.
+
+            // Note: We do this at the end to avoid creating a reentrancy vector.
+            // Refund the user any ETH they spent over the current price of the NFT.
+            // Unchecked is safe here because we validate msg.value >= price above.
+            SafeTransferLib.safeTransferETH(msg.sender, msg.value - price);
+        }
+        super.acquireProposer();
     }
 }
