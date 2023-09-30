ef058db788d6b361c8c8625b56f51499f78d6e7d
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-30 13:45:44 +0900

    Create ProposerManager.sol

---
+++ src/ProposerManager.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {IL2OutputOracle} from "./IL2OutputOracle.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+abstract contract ProposerManager is ERC721 {
+    IL2OutputOracle public immutable l2OutputOracle; // The L2 output oracle.
+
+    uint256 public totalSold; // The total number of tokens sold so far.
+
+    uint256 public immutable startTime = block.timestamp; // When VRGDA sales begun.
+
+    constructor(address l2OutputOracleAddr) ERC721("MyToken", "MTK") {
+        l2OutputOracle = IL2OutputOracle(l2OutputOracleAddr);
+    }
+
+    function proposeL2Output(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1BlockHash, uint256 _l1BlockNumber)
+        external
+        payable
+    {
+        l2OutputOracle.proposeL2Output{value: msg.value}(_outputRoot, _l2BlockNumber, _l1BlockHash, _l1BlockNumber);
+    }
+
+    function acquireProposer() external virtual returns (uint256 id);
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "https://example.com";
+    }
+}
