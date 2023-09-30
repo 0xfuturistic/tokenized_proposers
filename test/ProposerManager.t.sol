60683317167a8054f88502581019d9991c60fe9c
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-30 14:45:44 +0900

    create test for ProposerManager

---
+++ test/ProposerManager.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ProposerManager} from "../src/ProposerManager.sol";
+import {ERC6551Registry} from "erc6551/ERC6551Registry.sol";
+import {SimpleERC6551Account} from "erc6551/examples/simple/SimpleERC6551Account.sol";
+
+contract ProposerManagerTest is Test {
+    ProposerManager public manager;
+
+    event L2OutputProposed(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1BlockHash, uint256 _l1BlockNumber);
+
+    function setUp() public {
+        address l2OutputOracle = address(this);
+        address erc6551Registry = address(new ERC6551Registry());
+        address proposerAccountImpl = address(new SimpleERC6551Account());
+        manager = new ProposerManager(l2OutputOracle, erc6551Registry, proposerAccountImpl);
+    }
+
+    function test_acquireProposer() public {
+        manager.acquireProposer();
+    }
+
+    function proposeL2Output(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1BlockHash, uint256 _l1BlockNumber)
+        external
+    {
+        require(msg.sender == address(manager), "NOT_PROPOSER_MANAGER");
+        emit L2OutputProposed(_outputRoot, _l2BlockNumber, _l1BlockHash, _l1BlockNumber);
+    }
+}
